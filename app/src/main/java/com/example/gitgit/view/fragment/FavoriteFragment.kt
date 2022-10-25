55a56999f4bbe5e1fc1e0962c30a7a0bbbdf67f1
Tmdhoon2 <hks026naver@gmail.com>
2022-10-25 13:18:12 +0900

    ♻️ refactor :: change file name

--- app/src/main/java/com/example/gitgit/view/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/gitgit/view/fragment/FavoriteFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.View
 import com.example.gitgit.R
 import com.example.gitgit.base.BaseFragment
-import com.example.gitgit.databinding.FragmentFavoritteBinding
+import com.example.gitgit.databinding.FragmentHomeBindingImpl
 
-class FavoriteFragment : BaseFragment<FragmentFavoritteBinding>(R.layout.fragment_favoritte) {
+class FavoriteFragment : BaseFragment<FragmentHomeBindingImpl>(R.layout.fragment_favorite) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
