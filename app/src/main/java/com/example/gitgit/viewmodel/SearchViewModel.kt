4f293add132a4d6c39d2a57808e350ef2494c792
Tmdhoon2 <hks026naver@gmail.com>
2022-10-26 21:14:38 +0900

    ✨ feat :: implement star preferences logic

--- app/src/main/java/com/example/gitgit/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/gitgit/viewmodel/SearchViewModel.kt
@@ -1,21 +1,30 @@
 package com.example.gitgit.viewmodel
 
+import android.content.SharedPreferences
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.gitgit.R
+import com.example.gitgit.getPref
 import com.example.gitgit.repository.SearchRepository
 import com.example.gitgit.model.UserResponse
+import com.example.gitgit.putPref
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class SearchViewModel(private val searchRepository: SearchRepository) : ViewModel() {
+class SearchViewModel(private val searchRepository: SearchRepository, val pref: SharedPreferences) : ViewModel() {
     private val _userResponse: MutableLiveData<Response<UserResponse>> = MutableLiveData()
     val userResponse: LiveData<Response<UserResponse>> = _userResponse
 
     private val _createdAt : MutableLiveData<String> = MutableLiveData()
     val createdAt : LiveData<String> = _createdAt
+
+    private val _favorite : MutableLiveData<Int> = MutableLiveData()
+    val favorite : LiveData<Int> = _favorite
+
+    private lateinit var login : String
 
     fun search(userId: String) {
         viewModelScope.launch {
@@ -25,13 +34,29 @@ class SearchViewModel(private val searchRepository: SearchRepository) : ViewMode
                 val response = searchRepository.search(userId)
                 if(response.isSuccessful){
                     _userResponse.value = response
-                    Log.d("TEST", response.code().toString())
                     _createdAt.value = response.body()?.created_at?.split("T")?.get(0)
+                    login = response.body()!!.login
+                    Log.d("TEST", getPref(pref, login, false).toString())
+                    if(getPref(pref, login, false) as Boolean){
+                        _favorite.value = R.drawable.ic_star_yellow
+                    }else{
+                        _favorite.value = R.drawable.ic_star
+                    }
                 }
-            }.onFailure {exception ->
-                Log.d("TEST", exception.message.toString())
+            }.onFailure {
 
             }
+        }
+    }
+
+    fun favorite(){
+        Log.d("TEST", getPref(pref, login, false).toString())
+        if(getPref(pref, login, false) as Boolean){
+            _favorite.value = R.drawable.ic_star
+            putPref(pref.edit(), login, false)
+        }else{
+            _favorite.value = R.drawable.ic_star_yellow
+            putPref(pref.edit(), login, true)
         }
     }
 
--- app/src/main/java/com/example/gitgit/viewmodel/factory/SearchViewModelFactory.kt
+++ app/src/main/java/com/example/gitgit/viewmodel/factory/SearchViewModelFactory.kt
@@ -1,12 +1,13 @@
 package com.example.gitgit.viewmodel.factory
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.gitgit.repository.SearchRepository
 import com.example.gitgit.viewmodel.SearchViewModel
 
-class SearchViewModelFactory(private val searchRepository: SearchRepository) : ViewModelProvider.Factory {
+class SearchViewModelFactory(private val searchRepository: SearchRepository, private val sharedPreferences: SharedPreferences) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return SearchViewModel(searchRepository) as T
+        return SearchViewModel(searchRepository, sharedPreferences) as T
     }
 }
